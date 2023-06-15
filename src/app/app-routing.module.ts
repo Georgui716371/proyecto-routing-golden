5952d9e2a8c8ef61989596ad9d904b5a5314f58a
Georgui716371 <azelps071del@gmail.com>
2023-06-15 11:57:14 -0500

    primer Cambio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'dashboard', pathMatch:'full'},
+  {path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  {path:'principal', loadChildren: () => import('./components/principal/principal.module').then(m => m.PrincipalModule) },
+];
 
-@NgModule({
+
+  @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
